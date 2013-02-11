53d41725b3b9faf726f8fd1b166c275042e2f89d
dparshenko <dparsh@softserveinc.com>
2013-02-12 00:11:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+dotfiles
+========
+
+To Do
+-----
+* installation script
+* add my git mergetool to bin
+* make findorstart script work with terminal applications
+
+What goes where
+---------------
+synapse ~/.config
+tint2 ~/.config
+compton.conf ~/.config
+openbox ~/.config
+docky-2 ~/.gconf/apps
+.gitconfig ~
+bin ~
+.gtkrc-2.0 ~
+.gtkrc-2.0.mine ~
+.themes ~
+.conkyrc ~
+
+.bashrc
+-----------------------------------------
+    export PS1='\[\e[0;34m\]\w\$\[\e[0m\] '
+    alias chrome="google-chrome"
+    alias termin="/usr/bin/terminator"
+
+Just a note for findorstart
+---------------------------
+    wmctrl -lx
+    wmctrl -i -a 0x03a00005
+
---
+++ openbox/autostart.sh
@@ -0,0 +1,27 @@
+(sleep 2s && tint2) &
+setxkbmap -layout us,ru -variant -option grp:caps_toggle,grp_led:caps,terminate:ctrl_alt_bksp &
+
+# Make GTK apps look and behave how they were set up in the gnome config tools
+if test -x /usr/libexec/gnome-settings-daemon >/dev/null; then
+  /usr/libexec/gnome-settings-daemon &
+elif which gnome-settings-daemon >/dev/null; then
+  gnome-settings-daemon &
+# Make GTK apps look and behave how they were set up in the XFCE config tools
+elif which xfce-mcs-manager >/dev/null; then
+  xfce-mcs-manager n &
+fi
+
+# Preload stuff for KDE apps
+if which start_kdeinit >/dev/null; then
+  LD_BIND_NOW=true start_kdeinit --new-startup +kcminit_startup &
+fi
+
+gnome-sound-applet &
+
+synapse &
+
+nitrogen --restore &
+
+compton &
+
+docky &
